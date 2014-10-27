4c0a5d7a3a103b9164b1560d770c45845fcc4a0b
Maxim Dementyev <max@orofarne.ru>
2014-10-27 15:16:26 +0300

    Fix directory structure

--- update_geojson.bash
+++ update_geojson.bash
@@ -2,6 +2,7 @@
 
 for dir in `find -name '*.shp' | xargs dirname`
 do
-	target="`echo "$dir" | sed 's#^\./##'`.geojson"
+	target="`echo "$dir" | sed 's#/shapes/#/geojson/#'`.geojson"
+	rm -f "$target"
 	ogr2ogr -f GeoJSON "$target" "$dir"
 done
